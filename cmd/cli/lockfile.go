da1204ff7108c211ca780f8c04b443dfe0bff3ac
Dugi <dugisurdulli@gmail.com>
2022-09-07 20:36:07 -0400

    Prepare Lockfile api for consumption

--- cmd/cli/lockfile.go
+++ cmd/cli/lockfile.go
@@ -4,18 +4,65 @@ import (
 	"encoding/json"
 	"log"
 	"os"
+	"os/user"
+	"path"
 )
+
+type singleProjectJson map[string]int64
+type lockFileJson map[string]singleProjectJson
 
 type Lockfile struct {
 	Files []string
+	JSON  lockFileJson
 }
 
-func (l *Lockfile) GenerateLockfile() {
-	lockfileMapCh := make(chan map[string]int64)
-	go l.getFileModifiedMap(lockfileMapCh)
-	lockfileMap := <-lockfileMapCh
+func (l *Lockfile) Bootstrap() {
+	lockfilePath, err := l.getLockfilePath()
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-	jsonString, err := json.MarshalIndent(lockfileMap, "", "  ")
+	if !FileExists(lockfilePath) {
+		l.generateLockfile(nil, true)
+	}
+
+	currentLockFile, err := os.ReadFile(lockfilePath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = json.Unmarshal(currentLockFile, &l.JSON)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func (l *Lockfile) GetCurrentProject() singleProjectJson {
+	cwd, _ := os.Getwd()
+	return l.JSON[cwd]
+}
+
+func (l *Lockfile) UpdateTimestampsForFiles(files []string) {
+	lockfileMap := l.prepareMap(files)
+	cwd, _ := os.Getwd()
+	l.JSON[cwd] = lockfileMap
+	l.generateLockfile(files, false)
+}
+
+func (l *Lockfile) generateLockfile(files []string, initialLockfile bool) {
+	if files == nil {
+		files = l.Files
+	}
+
+	lockfileMap := l.prepareMap(files)
+
+	contents := l.JSON
+	if initialLockfile {
+		cwd, _ := os.Getwd()
+		contents = lockFileJson{cwd: lockfileMap}
+	}
+
+	jsonString, err := json.MarshalIndent(contents, "", "  ")
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -28,10 +75,16 @@ func (l *Lockfile) GenerateLockfile() {
 	}
 }
 
-func (l *Lockfile) getFileModifiedMap(ch chan map[string]int64) {
-	lockfileMap := make(map[string]int64)
+func (l *Lockfile) prepareMap(files []string) singleProjectJson {
+	lockfileMapCh := make(chan singleProjectJson)
+	go l.getFileModifiedMap(files, lockfileMapCh)
+	return <-lockfileMapCh
+}
 
-	for _, f := range l.Files {
+func (l *Lockfile) getFileModifiedMap(files []string, ch chan singleProjectJson) {
+	lockfileMap := make(singleProjectJson)
+
+	for _, f := range files {
 		fo, err := os.Stat(f)
 
 		if err != nil {
@@ -45,7 +98,14 @@ func (l *Lockfile) getFileModifiedMap(ch chan map[string]int64) {
 }
 
 func (l *Lockfile) writeLockfile(contents []byte, ch chan error) {
-	err := os.WriteFile("goke.lock", contents, 0644)
+	gokePath, err := l.getLockfilePath()
+
+	if err != nil {
+		ch <- err
+		return
+	}
+
+	err = os.WriteFile(gokePath, contents, 0644)
 
 	if err != nil {
 		ch <- err
@@ -53,4 +113,13 @@ func (l *Lockfile) writeLockfile(contents []byte, ch chan error) {
 	}
 
 	ch <- nil
+}
+
+func (l *Lockfile) getLockfilePath() (string, error) {
+	user, err := user.Current()
+	if err != nil {
+		return "", err
+	}
+
+	return path.Join(user.HomeDir, ".goke"), nil
 }
--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -6,6 +6,9 @@ func main() {
 	p := Parser{}
 	p.Bootstrap()
 
+	l := Lockfile{Files: p.FilePaths}
+	l.Bootstrap()
+
 	e := Executer{Parser: p}
 	if len(os.Args) > 1 {
 		e.Execute(os.Args[1])
